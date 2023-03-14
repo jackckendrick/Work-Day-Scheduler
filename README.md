d9fa49a5f5549b2e23e019b081527d1544d5ec3a
Jack Kendrick <jackkendrick@Jacks-MacBook-Pro.local>
2023-03-14 12:00:05 -0500

    added the correct JavaScript code and edited the HTML to add a time-block for every hour.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Work-Day-Scheduler
 This repository contains the code for an hourly work planner. 
+
+The work-day-scheduler is to be used as a daily planner to create an hourly schedule. When the planner is opened, the day is displayed at the top of the calendar. Upon scrolling down, the user is presented with hourly time-blocks. Each hour is color-coded indicating whether it is in the past, present, or future. When the user clicking into a time-bllock, they can type their tasks and save them in the local storage by clicking the save button. When the page is refreshed, the the task will still appear in the corresponding time-block. 
+
+The deployed webpage should resemble the following animation: <link href="./assets/worday-scheduler.gif"/>
+
+Deployed Link:
+
+Repository Link: 
--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var today = dayjs().format('MMMM D, YYYY h:mm A')
+var saveBtn = $(".saveBtn")
+var hour = $('.hour')
+var description = $('.description')
+
+
+
+$(document).ready (function runScheduler() {
   // TODO: Add a listener for click events on the save button. This code should
+ saveBtn.on("click", function(){
+  var hourDescription = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+  localStorage.setItem(time, hourDescription);
+ });
+
+ $("#0 .description").val(localStorage.getItem("0"))
+ $("#1 .description").val(localStorage.getItem("1"))
+ $("#2 .description").val(localStorage.getItem("2"))
+ $("#3 .description").val(localStorage.getItem("3"))
+ $("#4 .description").val(localStorage.getItem("4"))
+ $("#5 .description").val(localStorage.getItem("5"))
+ $("#6 .description").val(localStorage.getItem("6"))
+ $("#7 .description").val(localStorage.getItem("7"))
+ $("#8 .description").val(localStorage.getItem("8"))
+ $("#9 .description").val(localStorage.getItem("9"))
+ $("#10 .description").val(localStorage.getItem("10"))
+ $("#11 .description").val(localStorage.getItem("11"))
+ $("#12 .description").val(localStorage.getItem("12"))
+ $("#13 .description").val(localStorage.getItem("13"))
+ $("#14 .description").val(localStorage.getItem("14"))
+ $("#15 .description").val(localStorage.getItem("15"))
+ $("#16 .description").val(localStorage.getItem("16"))
+ $("#17 .description").val(localStorage.getItem("17"))
+ $("#18 .description").val(localStorage.getItem("18"))
+ $("#19 .description").val(localStorage.getItem("19"))
+ $("#20 .description").val(localStorage.getItem("20"))
+ $("#21 .description").val(localStorage.getItem("21"))
+ $("#22 .description").val(localStorage.getItem("22"))
+ $("#23 .description").val(localStorage.getItem("23"))
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -10,6 +47,28 @@ $(function () {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
+  function changeTime() {
+    var currentHour = dayjs().format('H');
+    console.log(currentHour)
+
+    $(".time-block").each(function(){
+      var timeId = parseInt($(this).attr("id"));
+      
+      if (timeId < currentHour) {
+        $(this).addClass('past');
+      } else if (timeId == currentHour){
+        $(this).addClass("present");
+      } else {
+        $(this).addClass('future');
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    })
+
+  }
+  changeTime();
+  var timeInterval = setInterval(changeTime, 1000);
+
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
@@ -20,4 +79,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $('#currentDay').text(today)
 });
